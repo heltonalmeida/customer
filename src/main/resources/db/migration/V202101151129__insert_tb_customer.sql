5790218db6031359fdd646b33c490fcf5b3a7a3a
heltonalmeida <helton.depaulajf@hotmail.com>
2021-01-15 11:35:03 -0300

    Incluindo script de insert na tb_customer

---
+++ src/main/resources/db/migration/V202101151129__insert_tb_customer.sql
@@ -0,0 +1,5 @@
+INSERT INTO db_customer.tb_customer (name, sexo, dt_birth, id_city) VALUES ('Helton', 'M', '1994-01-15', 1);
+INSERT INTO db_customer.tb_customer (name, sexo, dt_birth, id_city) VALUES ('Helton', 'M', '1994-08-15', 2);
+INSERT INTO db_customer.tb_customer (name, sexo, dt_birth, id_city) VALUES ('João', 'M', '1994-05-15', 1);
+INSERT INTO db_customer.tb_customer (name, sexo, dt_birth, id_city) VALUES ('Maria', 'F', '1994-06-15', 2);
+INSERT INTO db_customer.tb_customer (name, sexo, dt_birth, id_city) VALUES ('Maria', 'F', '1994-07-15', 3);
